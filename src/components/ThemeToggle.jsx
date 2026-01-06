855531bbc5fdb60639953965bffd7d2065a4baff
joecarpentieri <joecarpentieri1@gmail.com>
2026-01-06 09:49:01 +0000

    - Adds method to switch between dark and light mode when clicking the icons, in ThemeToggle file.

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -6,6 +6,16 @@ export const ThemeToggle = () => {
 
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  const toggleTheme = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    }
+  }
 
-  return <button> {isDarkMode ? <Sun /> : <Moon />}</button>
+
+  return <button onClick={toggleTheme}> {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" /> : <Moon className="h-6 w-6 text-blue-900" />}</button>
 }

eca3a6840a3d9075663c36db6243014a83825361
joecarpentieri <joecarpentieri1@gmail.com>
2026-01-06 10:13:48 +0000

    - Corrects functionality of cn in utils.js. Changes from const to function and has arguments in function.

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import { Sun, Moon } from "lucide-react";
 import { useEffect, useState } from "react";
+import { cn } from "../lib/utils";
 
 
 export const ThemeToggle = () => {
@@ -27,8 +28,24 @@ export const ThemeToggle = () => {
       localStorage.setItem("theme", "dark"); /* adding to local storage that dark is the current theme */
       setIsDarkMode(true);
     }
-  }
+  };
 
 
-  return <button onClick={toggleTheme}> {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" /> : <Moon className="h-6 w-6 text-blue-900" />}</button>
-}
+  return (
+    <button
+      onClick={toggleTheme}
+      className={cn(
+        "fixed top-5 right-5 z-50 p-2 rounded-full",
+        "hidden sm:flex items-center justify-center",
+        "transition-colors duration-300 focus:outline-none"
+      )}
+    >
+
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+        ) : (
+        <Moon className="h-6 w-6 text-blue-900" />
+        )}
+    </button>
+  );
+};

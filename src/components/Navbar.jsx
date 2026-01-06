6177012a3f5d3da7907221931374119cead231d1
joecarpentieri <joecarpentieri1@gmail.com>
2026-01-06 12:12:52 +0000

    - Adds desktop navigation.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,14 +20,28 @@ export const Navbar = () => {
     window.addEventListener("scroll", handleScroll)
     return () => window.removeEventListener("scroll", handleScroll);
   }, [])
-  return <div className={cn("fixed w-full z-40 transition-all duration-300", isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5")}>
+  return ( 
+  <nav
+  className={cn("fixed w-full z-40 transition-all duration-300", isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5")}>
 
     <div className="container flex items-center justify-between">
-      <a className="text-xl font-bold text-primary flex items-center">
+      <a className="text-xl font-bold text-primary flex items-center"
+      href="#hero">
         <span className="relative z-10">
-          <span className="text-glow"> Joe Carpentieri</span> Portfolio
+          <span className="text-glow text-foreground"> Joe Carpentieri</span> Portfolio
         </span>
       </a>
+
+      {/* desktop navigation */}
+      <div className="hidden md:flex space-x-8">
+        {navItems.map((item, key) => (
+          <a key={key} href={item.href} className="text-foreground/80 hover:text-primary transition-colors duration-300">
+            {item.name}</a>
+        ))}
+      </div>
+
+      {/* mobile navigation */}
     </div>
-  </div>
+  </nav>
+  );
 };

ad167c481f83521f2513b1e9b1102969aa6dca49
joecarpentieri <joecarpentieri1@gmail.com>
2026-01-06 09:43:32 +0000

    - Removed spaces between values for rgba so errors don't happen. Added new components folder inside source. Added 'ThemeToggle' file inside components folder, which has ThemeToggle function, which will toggle between light and dark themes with icons.

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,11 @@
+import { Sun, Moon } from "lucide-react";
+import { useState } from "react";
+
+
+export const ThemeToggle = () => {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+
+  return <button> {isDarkMode ? <Sun /> : <Moon />}</button>
+}
